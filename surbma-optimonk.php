b610dbb751c650d0f510003cae803a4c80041d8f
Surbma <peter@surbma.hu>
2020-02-10 14:54:22 +0100

    2.1

--- surbma-optimonk.php
+++ surbma-optimonk.php
@@ -5,7 +5,7 @@ Plugin Name: Surbma | OptiMonk
 Plugin URI: https://surbma.com/wordpress-plugins/
 Description: OptiMonk for WordPress
 
-Version: 2.0
+Version: 2.1
 
 Author: Surbma
 Author URI: https://surbma.com/
